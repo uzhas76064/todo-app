75b421c829fcc91afbb823cadddd115f022f428f
Maksim Hvatov <uzhas76064@gmail.com>
2020-09-06 14:08:32 +0500

    Updates

---
+++ precache-manifest.4883d9f05a7c9082b5f3dc78238b6505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cf81a60f5df489856b61b91dc152cd",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "26cd803d01c0c9cbe527",
+    "url": "/todo-app/static/css/main.bfcad14d.chunk.css"
+  },
+  {
+    "revision": "779f9659f73ad2aff2de",
+    "url": "/todo-app/static/js/2.f4450170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.f4450170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26cd803d01c0c9cbe527",
+    "url": "/todo-app/static/js/main.c0158b26.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
